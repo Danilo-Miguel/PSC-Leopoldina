71632c0308c3254494cb87faab2d427984f6b57c
Danilo-Miguel <dan.sm.mineiro@gmail.com>
2024-03-12 20:23:16 -0300

    Projeto iniciado

---
+++ PSC-Leopoldina/src/main/java/com/mycompany/psc/leopoldina/OlaMundo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.psc.leopoldina;
+
+/**
+ *
+ * @author Samsung
+ */
+public class OlaMundo {
+
+    public static void main(String[] args) {
+        System.out.println("Olá mundo!");
+    }
+}
