6e2805c3235acb56dcb9ba086cddad601ee8557c
Danilo de Souza Miguel <68231382+Danilo-Miguel@users.noreply.github.com>
2024-03-26 22:44:02 +0000

    Estrutura de selaeção

---
+++ estruturaDeSelecao/EstrturaDeSelecaoSwitch.java
@@ -0,0 +1,27 @@
+public class EstrturaDeSelecaoSwitch {
+    public static void main(String[] args){
+
+        int nota = 70;
+
+        switch(nota){
+            case 70:
+            System.out.println("Aprovado com conceito C");
+            break;
+
+            case 80:
+            System.out.println("Aprovado com conceito B");
+            break;
+
+            
+            case 90:
+            System.out.println("Aprovado com conceito A");
+            break;
+
+            default:
+            System.out.println("Reprovado");
+            
+
+
+        }
+   
+}
