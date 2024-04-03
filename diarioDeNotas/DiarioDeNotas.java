1af2b5af444d1be01ced04fc684dc54413643816
Danilo de Souza Miguel <68231382+Danilo-Miguel@users.noreply.github.com>
2024-04-03 00:02:05 +0000

    Criação da classediario de notas

---
+++ diarioDeNotas/DiarioDeNotas.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class DiarioDeNotas{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite 'Aluno' se você for um aluno e 'Professor' se você um professor" );
+        String usuario  = sc.nextLine();
+
+         if(usuario.equalsIgnoreCase("Professor")){
+            System.out.println("Digite o seu nome");
+            String nome  = sc.nextLine();
+            System.out.println("Ola Professor" + nome + "seja bem vindo ");
+
+         }else if(usuario.equalsIgnoreCase("Aluno")){
+            System.out.println("Digite o seu nome");
+            String nome  = sc.nextLine();
+            System.out.println("Ola Aluno " + nome + " seja bem vindo ");
+
+         }else{
+            System.out.println("Opção invalida ");
+
+         }
+         sc.close();
+
+    }
+
+}
