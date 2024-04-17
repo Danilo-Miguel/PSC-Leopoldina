c3ce92b5cfdec633f99462347ed5f1c0990a0d08
Danilo de Souza Miguel <68231382+Danilo-Miguel@users.noreply.github.com>
2024-04-17 00:56:32 +0000

    Chamada dos novos métodos

--- diarioDeNotas/DiarioDeNotas.java
+++ diarioDeNotas/DiarioDeNotas.java
@@ -2,23 +2,34 @@ import java.util.Scanner;
 
 public class DiarioDeNotas{
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Digite 'Aluno' se você for um aluno e 'Professor' se você um professor" );
-        String usuario  = sc.nextLine();
+        String usuario  = scanner.nextLine();
 
          if(usuario.equalsIgnoreCase("Professor")){
          Professor professor = new Professor();
-         professor.identificarProfessor();
+         professor.imprimir();
+         professor.identificarProfessor(usuario);
+
+         System.out.println("O que tem aqui Get?"  + professor.getNome());
+
+         //professor.somarNotas(nota1, nota2, nota3);
 
          }else if(usuario.equalsIgnoreCase("Aluno")){
+
+
+
             Aluno aluno = new Aluno();
-            aluno.identificarAluno();
+            String nome = aluno.identificarAluno(usuario);
+
+            System.out.println("Seja bem vindo " + usuario + nome);
 
          }else{
             System.out.println("Opção invalida ");
 
          }
-
+         scanner.close();
     }
+  
 
 }
