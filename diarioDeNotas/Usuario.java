fa15f14b09fff0826ce8a82ebfe0d0b6316136c5
Danilo de Souza Miguel <68231382+Danilo-Miguel@users.noreply.github.com>
2024-04-23 23:01:12 +0000

    Criação da classe usuário

---
+++ diarioDeNotas/Usuario.java
@@ -0,0 +1,42 @@
+public class Usuario {
+    private String nome;
+    private int idade;
+    private String email;
+    private String tipoUsuario;
+
+    public String getNome(){
+        return nome;
+       }
+    
+       
+       public void setNome(String nome){
+        this.nome = nome ;
+       }
+    
+       public int getIdade(){
+        return idade;
+       }
+    
+       
+       public void setIdade(int idade){
+        this.idade = idade ;
+       }
+    
+       public String getEmail(){
+        return email;
+       }
+    
+       
+       public void setEmail(String email){
+        this.email = email ;
+       }
+
+       public String getTipoUsuario(){
+        return tipoUsuario;
+       }
+    
+       
+       public void setTipoUsuario(String nome){
+        this.tipoUsuario = tipoUsuarioome ;
+       }
+}
